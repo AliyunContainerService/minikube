ada156ad646905e3ed22cdc95bd34586991949e4
Li Yi <denverdino@gmail.com>
2020-07-12 21:25:54 +0800

    Update to v1.12.0
    Support v1.12.3
    
    Update to v1.13.0
    
    Update to v1.13.0

--- pkg/minikube/notify/constants.go
+++ pkg/minikube/notify/constants.go
@@ -18,5 +18,5 @@ package notify
 
 const (
 	// GithubMinikubeReleasesURL is the URL of the minikube github releases JSON file
-	GithubMinikubeReleasesURL = "https://storage.googleapis.com/minikube/releases.json"
+	GithubMinikubeReleasesURL = "https://kubernetes.oss-cn-hangzhou.aliyuncs.com/minikube/releases.json"
 )
